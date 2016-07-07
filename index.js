0a0ebedaf1fb7bb1d605c7eb78954a89d1ec440b
Sam Verschueren <sam.verschueren@gmail.com>
2016-07-07 22:23:56 +0200

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+const got = require('got');
+const alfy = require('alfy');
+
+const KEY = 'api-list';
+const search = new RegExp(`${process.argv[2]}`, 'i');
+const now = Date.now();
+
+const isExpired = timestamp => now - timestamp > 3600000;
+
+const getApiList = () => {
+	if (alfy.cache.has(KEY)) {
+		const data = alfy.cache.get(KEY);
+
+		if (!isExpired(data.timestamp)) {
+			return Promise.resolve(data.items);
+		}
+	}
+
+	return got('https://angular.io/docs/ts/latest/api/api-list.json', { json: true })
+		.then(res => {
+			const modules = Object.keys(res.body);
+			let items = [];
+
+			for (const module of modules) {
+				items = items.concat(res.body[module]);
+			}
+
+			alfy.cache.set(KEY, {items, timestamp: now});
+			return items;
+		})
+		.catch(err => {
+			if (alfy.cache.has(KEY)) {
+				// Use the cached data if something happened
+				return alfy.cache.get(KEY).items;
+			}
+
+			throw err;
+		});
+}
+
+getApiList()
+	.then(modules => {
+		const items = modules
+			.filter(x => search.test(x.title))
+			.map(x => ({
+				title: x.title,
+				subtitle: `${x.barrel} (${x.stability})`,
+				arg: `https://angular.io/docs/ts/latest/api/${x.path}`,
+				quicklookurl: `https://angular.io/docs/ts/latest/api/${x.path}`
+			}));
+
+		console.log(JSON.stringify({items}));
+	})
+	.catch(err => {
+		console.log(JSON.stringify({
+			items: [{
+				title: err.name,
+				subtitle: err.message,
+				valid: false,
+				text: {
+					copy: err.stack
+				}
+			}]
+		}));
+	});
---
+++ readme.md
@@ -0,0 +1,30 @@
+# alfred-ng2
+
+> [Alfred 3](https://www.alfredapp.com) workflow to search for Angular 2 API references
+
+<img src="screenshot.png" width="694">
+
+
+## Install
+
+[Install the workflow.](http://www.packal.org/workflow/ng2)
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `ng2`, <kbd>Enter</kbd>, and your query.
+
+Select an item and press <kbd>Enter</kbd> to go to its [angular.io](https://angular.io) documentation.<br>
+Press <kbd>Shift</kbd> to view the documentation in Quick Look.
+
+
+## Related
+
+- [alfred-npms](https://github.com/sindresorhus/alfred-npms) - Search for npm packages with npms.io
+
+
+## License
+
+MIT © [Sam Verschueren](https://github.com/SamVerschueren)
