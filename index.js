a7126b53936670739672202bb319654766d2ca52
Sam Verschueren <sam.verschueren@gmail.com>
2016-07-13 22:02:13 +0200

    use `alfy.fetch` and `alfy.inputMatches`

--- index.js
+++ index.js
@@ -2,65 +2,39 @@
 const got = require('got');
 const alfy = require('alfy');
 
-const KEY = 'api-list';
-const TYPES = ['class', 'const', 'decorator', 'directive', 'enum', 'function', 'interface', 'var']
-const search = new RegExp(`${alfy.input}`, 'i');
-const now = Date.now();
+const TYPES = ['class', 'const', 'var', 'let', 'decorator', 'directive', 'enum', 'function', 'interface'];
 
-const isExpired = timestamp => now - timestamp > 86400000;
+const getApiList = () => alfy.fetch('https://angular.io/docs/ts/latest/api/api-list.json', {maxAge: 86400000})
+	.then(result => {
+		const modules = Object.keys(result);
+		let items = [];
 
-const getApiList = () => {
-	if (alfy.cache.has(KEY)) {
-		const data = alfy.cache.get(KEY);
-
-		if (!isExpired(data.timestamp)) {
-			return Promise.resolve(data.items);
+		for (const module of modules) {
+			items = items.concat(result[module]);
 		}
-	}
 
-	return got('https://angular.io/docs/ts/latest/api/api-list.json', { json: true })
-		.then(res => {
-			const modules = Object.keys(res.body);
-			let items = [];
-
-			for (const module of modules) {
-				items = items.concat(res.body[module]);
-			}
-
-			alfy.cache.set(KEY, {items, timestamp: now});
-			return items;
-		})
-		.catch(err => {
-			if (alfy.cache.has(KEY)) {
-				// Use the cached data if something happened
-				return alfy.cache.get(KEY).items;
-			}
-
-			throw err;
-		});
-}
+		return items;
+	});
 
 getApiList()
 	.then(modules => {
-		const items = modules
-			.filter(x => search.test(x.title))
-			.map(x => {
-				const url = `https://angular.io/docs/ts/latest/api/${x.path}`;
+		const items = modules.map(x => {
+			const url = `https://angular.io/docs/ts/latest/api/${x.path}`;
 
-				return {
-					title: x.title,
-					autocomplete: x.title,
-					subtitle: `${x.barrel} - ${x.docType}`,
-					arg: url,
-					quicklookurl: url,
-					icon: TYPES.includes(x.docType) && {path: `./icons/${x.docType}.png`},
-					mods: {
-						alt: {
-							subtitle: x.stability
-						}
+			return {
+				title: x.title,
+				autocomplete: x.title,
+				subtitle: `${x.barrel} - ${x.docType}`,
+				arg: url,
+				quicklookurl: url,
+				icon: TYPES.includes(x.docType) && {path: `./icons/${x.docType}.png`},
+				mods: {
+					alt: {
+						subtitle: x.stability
 					}
-				};
-			});
+				}
+			};
+		});
 
-		alfy.output(items);
+		alfy.output(alfy.inputMatches(items, 'title'));
 	});
