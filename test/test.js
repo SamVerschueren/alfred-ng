058720b6d00431b6268e293d0e102a6e388bb6cd
Sam Verschueren <sam.verschueren@gmail.com>
2016-08-20 20:41:55 +0200

    :arrow_up: alfy-test@0.3.0

--- test/test.js
+++ test/test.js
@@ -1,29 +1,33 @@
 import test from 'ava';
-import m from 'alfy-test';
+import alfyTest from 'alfy-test';
+
+test.beforeEach(t => {
+	t.context.alfy = alfyTest();
+});
 
 test('`items` property is array', async t => {
-	const result = await m('opt');
+	const result = await t.context.alfy('opt');
 
 	t.true(Array.isArray(result));
 	t.true(result.length > 0);
 });
 
 test('result', async t => {
-	const result = await m('opt');
+	const result = await t.context.alfy('opt');
 	const selectOption = result.find(item => item.title === 'NgSelectOption');
 
 	t.deepEqual(selectOption, {
 		title: 'NgSelectOption',
 		autocomplete: 'NgSelectOption',
-		subtitle: '@angular/common - directive',
-		arg: 'https://angular.io/docs/ts/latest/api/common/index/NgSelectOption-directive.html',
-		quicklookurl: 'https://angular.io/docs/ts/latest/api/common/index/NgSelectOption-directive.html',
+		subtitle: '@angular/forms - directive',
+		arg: 'https://angular.io/docs/ts/latest/api/forms/index/NgSelectOption-directive.html',
+		quicklookurl: 'https://angular.io/docs/ts/latest/api/forms/index/NgSelectOption-directive.html',
 		icon: {
 			path: './icons/directive.png'
 		},
 		mods: {
 			alt: {
-				subtitle: 'experimental'
+				subtitle: 'stable'
 			}
 		}
 	});
